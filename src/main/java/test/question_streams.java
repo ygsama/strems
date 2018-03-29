983b0d6dfa4e4f8d9b66881460112edf2ed944f4
ygsama <ygsama@qq.com>
2018-03-29 17:33:18 +0800

    md

--- src/main/java/test/question_streams.java
+++ src/main/java/test/question_streams.java
@@ -7,22 +7,21 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import redis.clients.jedis.Jedis;
 import streams_dsl.MyEventTimeExtractor;
 
-import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-import static java.sql.Types.TIMESTAMP;
 
 public class question_streams {
     public static void main(String[] args){
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-question-info");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-question");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "slave02:9092");
 
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -30,64 +29,56 @@ public class question_streams {
         props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, MyEventTimeExtractor.class.getName());
 
         final StreamsBuilder builder = new StreamsBuilder();
+        final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        final Jedis jedis = new Jedis("140.143.206.106", 6379);
+        jedis.auth("");
 
-
-        KStream<String, String> source = builder.stream("zhihu_question");
+        KStream<String, String> source = builder.stream("question");
 
         /*
          * 从 topic 中取出 question 数据，计算完毕后放入 Redis 的有序集合中
          */
         source.foreach((String key, String value) -> {
-//            {
-//                'id':'45147755',
-//                    'answer':'3876',
-//                    'coment':'45',
-//                    'view':'7821408',
-//                    'follower':'12435',
-//                    'last_answer':'3878',
-//                    'last_coment':'45',
-//                    'last_follower':'12440',
-//                    'last_view':'7832938'
-//            }
+
             JSONObject jsonObject = JSONObject.parseObject(value);
+
             String id = jsonObject.getString("id");
-            String createTime = jsonObject.getString("create_ttime");
-            long coment = jsonObject.getLongValue("coment");
+            String createTime = jsonObject.getString("create_time");
+            String insertTime = jsonObject.getString("insert_time");
+            String lastInsertTime = jsonObject.getString("last_insert_time");
+
             long follower = jsonObject.getLongValue("follower");
             long view = jsonObject.getLongValue("view");
-            String insertTime = jsonObject.getString("inser_time");
 
-            long lastComent = jsonObject.getLongValue("last_coment");
             long lastFollower = jsonObject.getLongValue("last_follower");
             long lastView = jsonObject.getLongValue("last_view");
-            String lastInsertTime = jsonObject.getString("last_inser_time");
 
 //            ( ((t1.follower_count+1)*0.2 + LOG10(t1.view_count))    *
 //                    ((t1.follower_count-t2.follower_count+0.1)/log10(TIMESTAMP(t1.insert_time)-TIMESTAMP (t2.insert_time))*0.5 +
 //                            log10(t1.view_count-t2.view_count+0.1)/log10(TIMESTAMP(t1.insert_time)-TIMESTAMP (t2.insert_time))
 //                    ))*86400 / POW(unix_timestamp(now())/2-unix_timestamp(t0.create_time)/2 + 1,1.5) as "总分",
 
-            double baseScore = 0L;
-            double accelerationScore = 0L;
-            double denominator = 0L;
+            double score_base = ( follower + 1 ) * 0.2 + Math.log10( view );
+            double score_accelerationScore = 0L;
+            double score_denominator = 0L;
 
-            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            baseScore = ( follower + 1 ) * 0.2 + Math.log10( view );
             try {
-                accelerationScore = ((follower-lastFollower+0.1)/
+                score_accelerationScore = ((follower-lastFollower+0.1)/
                         Math.log10(fmt.parse(insertTime).getTime()/1000-fmt.parse(lastInsertTime).getTime()/1000)*0.5 +
                         Math.log10(view-lastView+0.1)/Math.log10(fmt.parse(insertTime).getTime()/1000-fmt.parse(lastInsertTime).getTime()/1000)
                 )*86400;
-                denominator = Math.pow(new Date().getTime()/2-fmt.parse(createTime).getTime()/2 + 1,1.5);
+                score_denominator = Math.pow(new Date().getTime()/2-fmt.parse(createTime).getTime()/2 + 1,1.5);
 
             } catch (ParseException e) {
                 e.printStackTrace();
             }
 
-            double sumScore =  baseScore * accelerationScore / denominator;
+            double sumScore =  score_base * score_accelerationScore / score_denominator;
+            System.out.println(id+" : "+sumScore);
+
+            // TODO
 
         });
-
 
 
 
@@ -101,6 +92,7 @@ public class question_streams {
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
+
                 streams.close();
                 latch.countDown();
             }
--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -6,9 +6,57 @@ import java.util.Date;
 
 public class test {
     public static void main(String[] args) throws ParseException {
+//        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+//        Date date = fmt.parse("2018-01-01 10:20:20");
+//        System.out.println(date.getTime()/1000);
+
+        //            {
+//                'id':'45147755',
+//                    'answer':'3876',
+//                    'coment':'45',
+//                    'view':'7821408',
+//                    'follower':'12435',
+//                    'last_answer':'3878',
+//                    'last_coment':'45',
+//                    'last_follower':'12440',
+//                    'last_view':'7832938'
+//            }
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = fmt.parse("2018-01-01 10:20:20");
-        System.out.println(date.getTime()/1000);
+        String id = "45147755";
+        String createTime = "2016-05-01 20:10:22";
+
+        long follower = 12435L;
+        long view = 7821408L;
+        String insertTime = "2018-03-27 10:58:46";
+
+        long lastFollower = 12435L;
+        long lastView = 7820423L;
+        String lastInsertTime = "2018-03-27 08:56:42";
+
+//            ( ((t1.follower_count+1)*0.2 + LOG10(t1.view_count))    *
+//                    ((t1.follower_count-t2.follower_count+0.1)/log10(TIMESTAMP(t1.insert_time)-TIMESTAMP (t2.insert_time))*0.5 +
+//                            log10(t1.view_count-t2.view_count+0.1)/log10(TIMESTAMP(t1.insert_time)-TIMESTAMP (t2.insert_time))
+//                    ))*86400 / POW(unix_timestamp(now())/2-unix_timestamp(t0.create_time)/2 + 1,1.5) as "总分",
+
+        double score_base = ( follower + 1 ) * 0.2 + Math.log10( view );
+        double score_accelerationScore = 0L;
+        double score_denominator = 0L;
+
+        try {
+            score_accelerationScore = ((follower-lastFollower+0.1)/
+                    Math.log10(fmt.parse(insertTime).getTime()/1000-fmt.parse(lastInsertTime).getTime()/1000)*0.5 +
+                    Math.log10(view-lastView+0.1)/Math.log10(fmt.parse(insertTime).getTime()/1000-fmt.parse(lastInsertTime).getTime()/1000)
+            )*86400;
+            score_denominator = Math.pow(new Date().getTime()/2-fmt.parse(createTime).getTime()/2 + 1,1.5);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        double sumScore =  score_base * score_accelerationScore / score_denominator;
+        
+        System.out.println(sumScore);
 
 
     }
